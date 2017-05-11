ad918b7525e29aa537ab886090866bee20a2f888
anndoko <handcuffanndo@gmail.com>
2017-05-11 10:27:41 +0800

    implement user login/logout/signup

---
+++ db/migrate/20170511021735_add_columns_to_user.rb
@@ -0,0 +1,11 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :nickname, :string
+    add_column :users, :address, :string
+    add_column :users, :phone, :string
+    add_column :users, :birthday, :string
+    add_column :users, :gender, :string
+    add_column :users, :is_admin, :boolean, default: false
+  end
+end
