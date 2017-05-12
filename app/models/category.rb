c4238b1a776cfeda3d6ef5d2d43ac84919fb3c88
anndoko <handcuffanndo@gmail.com>
2017-05-12 16:05:42 +0800

    validate category and product data

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
+  # 新增/修改欄位限制與提示 #
+  validates :name, presence: true
+
+  # 關聯 #
   belongs_to :category_group
   has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
+  # 新增/修改欄位限制與提示 #
+  validates :name, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :quantity, numericality: { greater_than_or_equal: 0 }
+  validates :category_id, presence: true
+  validates :brand_id, presence: true
+
+  # 關聯 #
   belongs_to :category
   belongs_to :brand
 end
