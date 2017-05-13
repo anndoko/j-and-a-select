d581df1aed216b2e0c549efadbc62bb9bf7cf7e5
anndoko <handcuffanndo@gmail.com>
2017-05-13 10:52:57 +0800

    add category_group to category pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -21,6 +21,7 @@ class Admin::CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
+    @category.category_group_id = params[:category_group_id]
 
     if @category.save
       redirect_to admin_categories_path
@@ -36,6 +37,7 @@ class Admin::CategoriesController < ApplicationController
 
   def update
     @category = Category.find(params[:id])
+    @category.category_group_id = params[:category_group_id]
 
     if @category.update(category_params)
       redirect_to admin_categories_path
