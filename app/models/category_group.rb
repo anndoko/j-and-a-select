e865009b6bf742e03430e157de9f884fa5a2ba1d
anndoko <handcuffanndo@gmail.com>
2017-05-14 16:53:51 +0800

    implement category_group MVC

--- app/models/category_group.rb
+++ app/models/category_group.rb
@@ -1,3 +1,18 @@
 class CategoryGroup < ApplicationRecord
+  # 新增/修改欄位限制與提示 #
+  validates :name, presence: true
+
+  # 關聯 #
   has_many :categories
+
+  # 發佈 / 隱藏 #
+  def publish!
+    self.is_hidden = false
+    self.save
+  end
+
+  def hide!
+    self.is_hidden = true
+    self.save
+  end
 end
