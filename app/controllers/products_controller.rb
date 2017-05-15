a8735a8428719b0f5194b19d7358ad3b4f8c23df
anndoko <handcuffanndo@gmail.com>
2017-05-15 18:37:51 +0800

    create menu partial for product#index and product#show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,9 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     # 商品圖片
     @product_images = @product.product_images.all
+    # 商品所屬的品牌/分類
+    @category_groups = CategoryGroup.all
+    @brands = Brand.all
   end
 
   # 加入購物車
