683acc7a3b9bbc5248fc6ca51cad3f5ab18224b9
anndoko <handcuffanndo@gmail.com>
2017-05-15 19:56:50 +0800

    revamp product show

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -24,6 +24,16 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [400, 400]
   end
 
+  # 商品資訊頁大圖 #
+  version :main do
+    process resize_to_fill: [600, 400]
+  end
+
+  # 商品資訊頁小圖 #
+  version :other do
+    process resize_to_fill: [200, 200]
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
