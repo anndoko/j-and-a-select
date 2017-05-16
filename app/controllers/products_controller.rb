85cd899138b1e5ac4dab8daf11942818d470748d
anndoko <handcuffanndo@gmail.com>
2017-05-17 02:13:05 +0800

    implement category/brand filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,29 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.published.paginate(:page => params[:page], :per_page => 12)
-    # 商品所屬的品牌/分類
+    # 商品類型 / 品牌
     @category_groups = CategoryGroup.published
     @brands = Brand.published
+
+    # 判斷是否篩選分類
+    if params[:category].present?
+      @category = params[:category]
+      @category_id = Category.find_by(name: @category)
+
+      @products = Product.where(:category => @category_id).published.paginate(:page => params[:page], :per_page => 12)
+
+    # 判斷是否篩選品牌
+    elsif params[:brand].present?
+      @brand = params[:brand]
+      @brand_id = Brand.find_by(name: @brand)
+
+      @products = Product.where(:brand => @brand_id).published.paginate(:page => params[:page], :per_page => 12)
+
+    # 預設顯示所有公開商品
+    else
+      @products = Product.published.paginate(:page => params[:page], :per_page => 12)
+    end
+
   end
 
   def show
