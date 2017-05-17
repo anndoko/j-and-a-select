206469f5659d3333cf7223a0d7c87bf23f19fc0e
anndoko <handcuffanndo@gmail.com>
2017-05-17 18:17:00 +0800

    implement order show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
+
   # 產生訂單
   def create
     @order = Order.new(order_params)
@@ -7,10 +8,27 @@ class OrdersController < ApplicationController
     @order.total = current_cart.total_price
 
     if @order.save
+
+      # 將購買車內容存入訂單
+      current_cart.cart_items.each do |cart_item|
+        order_item = OrderItem.new
+        order_item.order = @order
+        order_item.product_name = cart_item.product.name
+        order_item.product_price = cart_item.product.price
+        order_item.quantity = cart_item.quantity
+        order_item.save
+      end
+
       redirect_to order_path(@order)
     else
       render 'carts/checkout'
     end
+  end
+
+  # 訂單明細
+  def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
 
   private
