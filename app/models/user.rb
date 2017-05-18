b7af034a773912ee7f0e29a3df4e779d6a082bad
anndoko <handcuffanndo@gmail.com>
2017-05-18 14:32:51 +0800

    implement is_wish_list_owner_of?(product)

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,15 @@ class User < ApplicationRecord
   has_many :wish_list_items, :through => :wish_lists, :source => :product
 
 
+  def is_wish_list_owner_of?(product)
+    wish_list_items.include?(product)
+  end
+
   # 檢查 is_admin 的 boolean 值
   def admin?
     is_admin
   end
+
+
 
 end
