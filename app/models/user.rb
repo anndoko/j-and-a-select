d40340fe684a5a5c91bcf0d0a216f10876f1632b
xbearx1987 <xbearx1987@gmail.com>
2017-05-18 16:44:32 +0800

    setup user#registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :nickname, presence: { message: "请输入用戶名" }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # 關聯 #
   has_many :orders
-
 
   # 檢查 is_admin 的 boolean 值
   def admin?
