e3e23ba7978759fe1ba1899da0c2d4ffd8a60dd1
anndoko <handcuffanndo@gmail.com>
2017-05-19 10:46:55 +0800

    fix carrierwave configuration

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,17 +1,16 @@
-CarrierWave.configure do |config|
-  if Rails.env.production?
+if Rails.env.production?
+  CarrierWave.configure do |config|
     config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV['AKIAINYHAN5RA3ZBAL2Q'], # Access Key Id
-
       aws_secret_access_key: ENV['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'], # Secret Access Key
-
       region:                'ap-northeast-1'
     }
     config.fog_directory  = 'j-and-a-select-bucket' # bucket name
-
-  else
+  end
+else
+  CarrierWave.configure do |config|
     config.storage :file
   end
 end
