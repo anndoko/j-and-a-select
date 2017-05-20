126ae4816a3b8f8d6aaf78bd51c702421478f0d8
xbearx1987 <xbearx1987@gmail.com>
2017-05-20 16:32:43 +0800

    setup admin sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170519184437) do
 
   create_table "brands", force: :cascade do |t|
