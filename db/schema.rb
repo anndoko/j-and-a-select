74c7a5a205b21a8ea80d99bfbab32eb1b86c01ee
xbearx1987 <xbearx1987@gmail.com>
2017-05-24 22:57:36 +0800

    add currency to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523035748) do
+ActiveRecord::Schema.define(version: 20170524145528) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20170523035748) do
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
     t.string   "aasm_state",       default: "order_placed"
+    t.string   "currency"
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
